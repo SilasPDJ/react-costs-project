446b9e8afd7fe6218ac4dc08233621899c95fce8
Silas B. Ferreira <33920489+SilasPDJ@users.noreply.github.com>
2023-08-03 14:31:22 -0300

    app.js

--- costs/src/App.js
+++ costs/src/App.js
@@ -6,22 +6,22 @@ import Contact from './components/pages/Contact';
 import Container from './components/pages/layout/Container';
 
 function App() {
-    return (
-        <Router>
-            <div>
-                <Link to="/">Home</Link>
-                <Link to="/contact">Contact</Link>
-                <Link to="/company">Company</Link>
-                <Link to="/new_project">new_project</Link>
-            </div>
-            <Routes>
-                <Route path="/" element={<Container><Home /></Container>} />
-                <Route path="/contact" element={<Container><Contact /></Container>} />
-                <Route path="/company" element={<Container><Company /></Container>} />
-                <Route path="/new_project" element={<Container><NewProject /></Container>} />
-            </Routes>
-        </Router>
-    );
+  return (
+      <Router>
+          <div>
+              <Link to="/">Home</Link>
+              <Link to="/contact">Contact</Link>
+              <Link to="/company">Company</Link>
+              <Link to="/new_project">new_project</Link>
+          </div>
+          <Routes>
+              <Route path="/" element={<Container><Home /></Container>} />
+              <Route path="/contact" element={<Container><Contact /></Container>} />
+              <Route path="/company" element={<Container><Company /></Container>} />
+              <Route path="/new_project" element={<Container><NewProject /></Container>} />
+          </Routes>
+      </Router>
+  );
 }
 
 export default App;
