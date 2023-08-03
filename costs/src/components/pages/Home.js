822461bacfbba064f744fcfefbaf6b34cdb3c2a6
Silas B. Ferreira <33920489+SilasPDJ@users.noreply.github.com>
2023-08-03 17:06:39 -0300

    jsconfig.json

--- costs/src/components/pages/Home.js
+++ costs/src/components/pages/Home.js
@@ -1,3 +1,16 @@
-export default function Home(){
-    return <h1>Home page</h1>
+import styles from './Home.module.css'
+import savings from "../../img/savings.svg"
+export default function Home() {
+  return (
+    <section className={styles.home_container}>
+      <h1>Bem vindo ao <span>Costs</span></h1>
+      <p>Comece a gerenciar os seus projetos agora mesmo!</p>
+      <a href="/">Criar projeto</a>
+      <img src={savings} alt="Costs" />
+
+    </section>
+
+
+
+  )
 }
