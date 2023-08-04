5ea1d9934a7a510b3e4388e3a4f4560f716fd663
Silas B. Ferreira <33920489+SilasPDJ@users.noreply.github.com>
2023-08-04 15:35:02 -0300

    init Projects.js #26

--- costs/src/components/pages/Projects.js
+++ costs/src/components/pages/Projects.js
@@ -1,5 +1,9 @@
 import { useLocation } from "react-router-dom"
+import styles from './Projects.module.css'
+
 import Message from "../layout/Message"
+import Container from "../layout/Container"
+import LinkButton from "../layout/LinkButton"
 
 export default function Projects() {
   const location = useLocation()
@@ -9,10 +13,17 @@ export default function Projects() {
     message = location.state.message
   }
   return (
-    <div>
-      <h1>Meus Projetos</h1>
+    <div className={styles.project_container}>
+      <div className={styles.title_container}>
+        <h1>Meus Projetos</h1>
+        <LinkButton to="/newproject" text="Criar Projeto" />
+      </div>
 
       {message && <Message msg={message} type='success' duration={3000} />}
+      <Container customClass="start">
+        <p>Projetos...</p>
+
+      </Container>
 
     </div>
   )
